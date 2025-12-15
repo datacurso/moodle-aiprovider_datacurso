7ea34b1e907ccbd9f1f434607075103d64ee78a9
JosueCondori-17 <josue@datacurso.com>
2025-12-15 11:14:48 -0500

    fix: add support in version

--- version.php
+++ version.php
@@ -26,6 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_datacurso';
 $plugin->release = '2.0.0';
+$plugin->supported = [500, 501];
 $plugin->version = 2025121200;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_STABLE;
