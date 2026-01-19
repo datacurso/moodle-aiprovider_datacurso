e38701bcb46082c6e7e4cc6df7163a57ac97e4d7
wilber-buendata <wilber@buendata.com>
2026-01-19 14:03:28 -0500

    chore: bump version to 2026011900 and release 1.0.6

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_datacurso';
-$plugin->release = '1.0.5';
-$plugin->version = 2025120301;
+$plugin->release = '1.0.6';
+$plugin->version = 2026011900;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405];
