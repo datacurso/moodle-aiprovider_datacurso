50809d8cc87363f38147b3012941073b923a34da
wilber-buendata <wilber@buendata.com>
2026-01-19 14:02:01 -0500

    fix: ensure boolean comparison in is_for_ue method
    (cherry picked from commit a6a7269ab9896f304991963243d082b430cb3cd4)

--- classes/httpclient/datacurso_api_base.php
+++ classes/httpclient/datacurso_api_base.php
@@ -339,6 +339,6 @@ class datacurso_api_base {
     public function is_for_ue(): bool {
         $datacursoapi = new datacurso_api();
         $response = $datacursoapi->get('tokens/saldo');
-        return $response['is_for_eu'];
+        return $response['is_for_eu'] == true;
     }
 }
