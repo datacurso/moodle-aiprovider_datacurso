7d4d951367df9d9a0b7b4b8185c0f417a50ac55f
wilber-buendata <wilber@buendata.com>
2026-04-29 21:32:15 -0500

    chore: align release metadata to 1.1.6
    - Merge allowlist-removal notes into the 1.1.6 changelog section
    
    - Set plugin release label to 1.1.6 while keeping build version 2026042902

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_datacurso';
-$plugin->release = '1.1.7';
+$plugin->release = '1.1.6';
 $plugin->version = 2026042902;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
