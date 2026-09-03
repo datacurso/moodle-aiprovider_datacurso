ab2289a0a27af6f5e65ce16bdc2001bd60b1047c
JosueCH-17 <josue@datacurso.com>
2026-09-03 09:42:19 -0500

    fix upload version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_datacurso';
 $plugin->release = '1.5.0';
-$plugin->version = 2026090102;
+$plugin->version = 2026090300;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405];
